221d7b7283da94fe03e5f675abe8ad0ff7fb4bfd
magico15-cmyk <chadlioussama15@gmail.com>
2026-06-29 19:45:19 +0100

    Fix hydration mismatch in font loading

--- src/app/[domain]/layout.tsx
+++ src/app/[domain]/layout.tsx
@@ -49,14 +49,7 @@ export default async function DomainLayout(props: {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       {/* Preload and async load the Google Fonts to prevent render blocking */}
       <link rel="preload" as="style" href={fontUrl} />
-      <link rel="stylesheet" href={fontUrl} media="print" id="dynamic-google-fonts" />
-      <script dangerouslySetInnerHTML={{ __html: `
-        // Non-blocking async CSS load
-        document.getElementById('dynamic-google-fonts').media = 'all';
-      `}} />
-      <noscript>
-        <link rel="stylesheet" href={fontUrl} />
-      </noscript>
+      <link rel="stylesheet" href={fontUrl} />
 
       {store && (
         <style dangerouslySetInnerHTML={{ __html: `
